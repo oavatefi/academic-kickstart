7774be21d8c47e529089178c34beaf6a1609ba43
kabdulgh <Kareem.abdulghany@valeo.com>
2017-10-04 16:56:05 +0200

    Fix Merging issue

--- project_sources_soc.mk
+++ project_sources_soc.mk
@@ -1,7 +1,14 @@
 # List common C source files here.
-
+P1_SPECIFIC_FILES:=$(TUAM_TRUNK)/sw/vayu/P1_Config
 project_path := $(shell dirname $(lastword $(MAKEFILE_LIST)))
 
-CSRC :=
-CXXSRC :=  
-EXTRAINCDIRS = $(project_path) $(project_path)/daimler_src
+FRAMEWORK_CSOURCES += \
+	$(P1_SPECIFIC_FILES)/Daimler/bmp/bmp_images.c
+FRAMEWORK_CXXSOURCES += \
+	$(P1_SPECIFIC_FILES)/Daimler/p1_ovl/p1_ovlrefstaticoverlayinfo.cpp
+
+EXTRAINCDIRS += \
+     $(P1_SPECIFIC_FILES)/Daimler/app_params \
+     $(project_path) $(project_path)/daimler_src
+ FRAMEWORK_INCDIRS += \
+     $(P1_SPECIFIC_FILES)/Daimler/app_params
