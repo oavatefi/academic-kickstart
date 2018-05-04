6602424fa48112921be662abbd40b5a6be2bb666
skreyer <steve.kreyer@valeo.com>
2018-05-04 13:41:55 +0200

    changing back to PARKMAN ECU

--- platform_config.mk
+++ platform_config.mk
@@ -4,7 +4,7 @@ project_path := $(shell dirname $(lastword $(MAKEFILE_LIST)))
 
 PROJECT_DIST_PATH := $(project_path)/dist
 #PLATFORM_ECU_TYPE := mfas
-PLATFORM_ECU_TYPE := parkman
+PLATFORM_ECU_TYPE := PARKMAN
 
 PROJECT_NAME := DAIMLER
 
