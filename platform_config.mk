a6aff5e960a9821d9378e0bc05dad20f6b2f2eb7
skreyer <steve.kreyer@valeo.com>
2018-05-15 19:37:56 +0200

    paas/ECU: corrected interface usage of send/rcv on non-blocking socket in reactor pattern

--- platform_config.mk
+++ platform_config.mk
@@ -3,7 +3,7 @@
 project_path := $(shell dirname $(lastword $(MAKEFILE_LIST)))
 
 PROJECT_DIST_PATH := $(project_path)/dist
-#PLATFORM_ECU_TYPE := mfas
+#PLATFORM_ECU_TYPE := MFAS
 PLATFORM_ECU_TYPE := PARKMAN
 
 PROJECT_NAME := DAIMLER
