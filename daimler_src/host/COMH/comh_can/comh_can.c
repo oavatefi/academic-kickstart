c7ff13b97b091b762fa7ddd90358568d81bee746
Yahya SAMEH <yahya.sameh@valeo.com>
2018-06-26 10:14:06 +0200

    removed comh_can arch dependant bounded GetCantTransState with linux/scan compilation flags edited makefiles to reflect these changes

--- daimler_src/host/COMH/comh_can/comh_can.c
+++ daimler_src/host/COMH/comh_can/comh_can.c
@@ -1,5 +1,5 @@
 /*
- * comh_can_common.c
+ * comh_can.c
  *
  *  Created on: May 24, 2018
  *      Author: ysameh
@@ -53,7 +53,7 @@
 /******************************************************************************/
 /*                            Include other headers                           */
 /******************************************************************************/
-
+#include "canwr.h"
 
 
 
@@ -151,6 +151,8 @@ enum CFG_send_warnelems_S
 struct CAPP_pos_S odom_pos = {{0,0},{0,0,0,0},0, 0, 0} ;
 #endif
 static enum CFG_send_warnelems_S warnelems_send_cfg = CFG_WE_DISABLED;
+static CanWR_CHANNEL_ID_E CAN_CHANNEL;
+
 /******************************************************************************/
 /*                     Definition of local constant data                      */
 /******************************************************************************/
@@ -177,6 +179,13 @@ static u8  P2GPA_sgws_recv_echos_mask[P2GPA_NUM_SIGNALWAYS_TOTAL];
 static void SetSDBGDists(u8 sgw, u16* p_dists_mm);
 #endif
 
+static void Can1Receive (u16 id, const u8* data, u8 dlc);
+static void Can1ReceiveExt(u32 id, const u8* data,u8 dlc);
+
+
+static void Can0Receive (u16 id, const u8* data, u8 dlc);
+static void Can0ReceiveExt(u32 id, const u8* data, u8 dlc);
+static void InitCanWrapper(void);
 
 
 /******************************************************************************/
@@ -210,10 +219,10 @@ static void SetSDBGDists(u8 sgw, u16* p_dists_mm)
 
 		}
 		else /* mod3_sgw == 2 */
-				{
+		{
 			sender = sgw+1;
 			receiver = sgw-2;
-				}
+		}
 
 		sender /= 3;
 		receiver /= 3;
@@ -237,10 +246,10 @@ static void SetSDBGDists(u8 sgw, u16* p_dists_mm)
 
 		}
 		else /* mod3_sgw == 2 */
-				{
+		{
 			sender = sgw+1;
 			receiver = sgw-2;
-				}
+		}
 
 		sender /= 3;
 		sender += 1;
@@ -267,6 +276,115 @@ static void SetSDBGDists(u8 sgw, u16* p_dists_mm)
 	}
 }
 #endif
+/*CAN wrapper specific functions*/
+
+
+static void Can1Receive (u16 id, const u8* data, u8 dlc)
+{
+	CanReceive(id, data, dlc);
+}
+static void Can1ReceiveExt(u32 id, const u8* data, u8 dlc)
+{
+	CanReceiveExt(id, data, dlc);
+}
+static void Can0Receive (u16 id, const u8* data, u8 dlc)
+{
+	CanReceive(id, data, dlc);
+}
+static void Can0ReceiveExt(  u32 id, const u8* data,  u8 dlc)
+{
+	CanReceiveExt(id, data, dlc);
+}
+enu_p2gpa_can_trans_state_T P2GPA_GetCanTransState(void)
+{
+#ifndef FW_USE_LINUX_ICAM
+
+	CanWr_trans_state_E trans_state = CanWr_GetCanTransState();
+	enu_p2gpa_can_trans_state_T p2gpa_trans_state;
+	switch(trans_state)
+	{
+	case CANWR_TRANS_OFF_STATE:
+		p2gpa_trans_state = P2GPA_CAN_TRANS_OFF_STATE;
+		break;
+	case CANWR_TRANS_ACTIVE_STATE:
+		p2gpa_trans_state =  P2GPA_CAN_TRANS_ACTIVE_STATE;
+		break;
+	default:
+		p2gpa_trans_state =CANWR_TRANS_OFF_STATE;
+	}
+	return p2gpa_trans_state;
+#endif
+}
+void InitCanWrapper(void)
+{
+
+	CanWR_CFG_T canwr_cfg;
+
+	/*define Filters*/
+	canwr_cfg.filterd_id_list[0] = 0xAA; /* Brk_Data_ESP */
+	canwr_cfg.filterd_id_list[1] = 0xA6; /* Ign_Veh_Stat_ESP */
+	canwr_cfg.filterd_id_list[2] = 0x9F; /* Park_Brk_Rs_Data_ESP */
+	canwr_cfg.filterd_id_list[3] = 0xAF; /* Veh_Accel_Data_ESP */
+	canwr_cfg.filterd_id_list[4] = 0x9E; /* Veh_Speed_Data_ESP */
+	canwr_cfg.filterd_id_list[5] = 0xA8; /* Whl_Stat_Left_ESP */
+	canwr_cfg.filterd_id_list[6] = 0xA7; /* Whl_Stat_Right_ESP */
+	canwr_cfg.filterd_id_list[7] = 0xA2; /* Gr_Current_Gear_CPC */
+	canwr_cfg.filterd_id_list[8] = 0xA5; /* PwrTr_Stat_CPC */
+	canwr_cfg.filterd_id_list[9] = 0xAE; /* TSL_Target_Pos_CPC */
+	canwr_cfg.filterd_id_list[10] = 0xAB; /* Buttons_Data_EIS */
+	canwr_cfg.filterd_id_list[11] = 0xA0; /* Electronic_Brk_Eng */
+	canwr_cfg.filterd_id_list[12] = 0xA1; /* Park_St_Rs_EPS*/
+	canwr_cfg.filterd_id_list[13] = 0xAD;  /* Steering_Data_EPS */
+	canwr_cfg.filterd_id_list[14] = 0xAC;  /* Turn_Indicators_Data_EIS */
+	canwr_cfg.filterd_id_list[15] = 0x98; /* VehDyn_Stat2_ESP */
+
+	/* Not used for Daimler BR213 */
+	canwr_cfg.filterd_id_list[16] = 0x51B; /* CLU_16 */
+	canwr_cfg.filterd_id_list[17] = 0x520;	/* CGW3 */
+	canwr_cfg.filterd_id_list[18] = 0x541;	/* CGW1 */
+	canwr_cfg.filterd_id_list[19] = 0x553;	/* CGW2 */
+	canwr_cfg.filterd_id_list[20] = 0x600;	/* PCA */
+	canwr_cfg.filterd_id_list[21] = 0x502;//0x644;	/* RSPA_C2 */
+	canwr_cfg.filterd_id_list[22] = 0x100;	/* P4U btns sim */
+	canwr_cfg.filterd_id_list[23] = XP2GPA_CAN_ID_PARA_RECEIVE;//0x100;//XISTP_RESP_CAN_ID;
+	canwr_cfg.filterd_id_list[24] = 0x3A5;
+	canwr_cfg.filterd_id_list[25] = 0x766; /*PCA_USS*/
+
+	canwr_cfg.filterd_id_list[26] = 0x6B7; /*PCA_USS*/
+	canwr_cfg.num_filtered_id = 27;
+	/*Set Tx confirmation Callback*/
+
+	canwr_cfg.call_back_list[0].tx_cmplt_call_back = PTPN_Apl_OnDataSent;
+	canwr_cfg.call_back_list[0].msg_id = 0x665;
+	canwr_cfg.num_call_back = 1;
+	canwr_cfg.channel_id = CAN_CHANNEL;
+
+#ifdef FW_USE_LINUX_ICAM
+	canwr_cfg.RecvCbk = Can0Receive;
+	canwr_cfg.RecvCbkExt = Can0ReceiveExt;
+#else
+	canwr_cfg.RecvCbk = Can1Receive;
+	canwr_cfg.RecvCbkExt = Can1ReceiveExt ;
+#endif
+
+	CanWR_Init(&canwr_cfg);
+}
+
+u8 P2GPA_CanSendDebugCh (u16 id, const u8 *data, u8 dlc)
+{
+
+	return CanWR_Tx(id, data, dlc, CAN_CHANNEL);
+
+}
+
+u8 P2GPA_CanSend (enum P2GPA_CAN_prio_E prio, u16 id, const u8 *p, u8 n)
+{
+	return CanWR_Tx(id, p, n, CAN_CHANNEL);
+}
+u8 P2GPA_CanSendExt (u32 id, const u8 *p, u8 n)
+{
+	return CanWR_TxExt(id, p, n, CAN_CHANNEL);
+}
 
 /******************************************************************************/
 /*                      Definition of exported functions                      */
@@ -285,19 +403,15 @@ void P2GPA_CanInit (void)
 		P2GPA_sgws_recv_echos_mask[temp_i] = 0;
 	}
 #endif
-
-/*For Linux*/
 #ifdef FW_USE_LINUX_ICAM
-	InitLinuxCan();
-
-/*For Tmpl SCAN*/
+	CAN_CHANNEL = CanWR_CHANNEL_ID_CAN0;
 #else
+	CAN_CHANNEL = CanWR_CHANNEL_ID_CAN1;
+#endif
 
-	InitSCan();
+	InitCanWrapper();
 
-#endif /*FW_USE_LINUX_ICAM/
-
-/*VS6*/
+	/*VS6*/
 #ifdef APPL_VS6_ECHOES_CAN
 	VS6_CanInit();
 #endif
