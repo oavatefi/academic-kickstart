13c9689ef0a93de28740bea2284b7e654988f0b0
mshalaby <mahmoud.shalaby@valeo.com>
2018-08-30 23:37:02 +0200

    added different cars definition in platform_project_pdf.h

--- daimler_src/platform_project_pdf.h
+++ daimler_src/platform_project_pdf.h
@@ -8,6 +8,11 @@
 #ifndef APPL_PLATFORM_PROJECT_PDF_H_
 #define APPL_PLATFORM_PROJECT_PDF_H_
 
+//MSH: 20180814: Clarify different vehicles that can be used ...
+//cfgh_set_br213_s_ml       ==> white kombi 283
+//cfgh_set_br213_lb_vs_117  ==> black demo car 117
+//cfgh_set                  ==> demo car tuam/troy (as per Malte)
+
 #define PLATFORM_project_Task10ms        COMH_Cyclic10ms
 #define PLATFORM_project_Task20ms
 #define PLATFORM_project_Task40ms
